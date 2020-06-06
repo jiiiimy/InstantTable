80f52b9a7e7791bcd7b22716aa8d410e85228480
Ryota Yajima <ryotayajima@gmail.com>
2020-06-06 12:52:27 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+let vm = new Vue({
+	el: '#all',
+	data: {
+		columnHead: [{
+			text: '',
+			delete: false,
+		}, {
+			text: '',
+			delete: true,
+		}],
+		row: [
+			[{
+				text: '',
+				delete: false,
+				head: true,
+				data: false,
+			}, {
+				text: '',
+				head: false,
+				data: true,
+			}, {
+				text: '',
+				head: false,
+				data: true,
+			}],[{
+				text: '',
+				delete: true,
+				head: true,
+				data: false,
+			}, {
+				text: '',
+				head: false,
+				data: true,
+			}, {
+				text: '',
+				head: false,
+				data: true,
+			}]
+		],
+	},
+	methods: {
+		addColumn: function () {
+			let col = { text: '', delete: true }
+			let data = { text: '', head: false, data: true}
+			this.columnHead.push(col);
+			for (let i = 0; i < this.row.length; i++) {
+				this.row[i].push(data);
+			}
+		},
+		deleteColumn: function (index) {
+			this.columnHead.splice(index, 1);
+			for (let i = 0; i < this.row.length; i++) {
+				this.row[i].splice(index + 1, 1);
+			}
+		},
+		addRow: function(){
+			let row = [{ text: '',delete: true, head: true, data: false }]
+			let data = { text: '', head: false, data: true }
+			for (let i = 0; i < this.row[0].length - 1; i++) {
+				row.push(data);
+			}
+			this.row.push(row);
+		},
+		deleteRow: function (index) {
+			this.row.splice(index, 1);
+		}
+	}
+});
